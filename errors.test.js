9501e70df05688ad2aa206080746fa2d78f06c4e
Jesper Linne <jesperlinne@hotmail.com>
2019-02-27 13:28:07 +0100

    Lagt upp alla filer

---
+++ errors.test.js
@@ -0,0 +1,38 @@
+const functions = require("./errors");
+
+
+test("Adds 2 + 2 to equal 4", () => {
+    expect(2 + 2).toBe(4);
+});
+
+test("Adds 2 + 2 to not equal 5", () => {
+    expect(2 + 2).not.toBe(5);
+});
+
+test("Adds 0.1 + 0.2 to equal 0.3", () => {
+    expect(0.1 + 0.2).toBeCloseTo(0.3);
+});
+
+const obj1 = {name:"test", data:[1,2,3]
+};
+const obj2 = {name:"test", data:[1,2,3]
+};
+
+test("checks if obj1 to be like obj2", () => {
+    expect(obj1).toEqual(obj2);
+});
+
+
+
+  describe('functions', () => {
+
+   test("check empty list", ()=> {
+          expect(functions.emptyList()).toEqual([]);
+      
+        });
+     test("check empty list", ()=> {
+            expect(functions.emptyList()).toEqual(0);
+        
+          });
+  });
+
